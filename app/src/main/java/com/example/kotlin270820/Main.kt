b23e765708e2c28c098db4599ad46fb92a5ce5ef
Eduardo <archivo.ehv@gmail.com>
2020-08-27 23:11:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/kotlin270820/Main.kt
@@ -0,0 +1,26 @@
+package com.example.kotlin270820
+
+fun main() {
+    // println("Hola Mundo Kotlin")
+
+
+    //Declarar y asignar una variable
+
+    var number : Int = 10
+
+    val number2 : Int = 20
+
+    var name = "Edo"
+
+    // Concatenar strings
+
+    val palabra01 = "Hola que tal"
+
+    val conatenado = "El valor de la palabra 01 es:  $number"
+    println(conatenado)
+
+    //Contar caracteres
+
+   val sinespacios = palabra01.replace("", "" )
+    val conatenado = "El valor de la palabra 01 es:  ${palabra01.tr}
+}
